e48fb0280c351b28f9f932cda12c95ff239cd9a0
Theeraphat <theeraphat.tr@gmail.com>
2024-12-19 01:51:06 +0700

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/api/:path*', // Apply to all API routes
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*', 
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, PATCH, DELETE, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
